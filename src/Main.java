dcc581d1aebb089bfa7156be9c84e9da33bb2163
enesJav16 <maratuenes@gmail.com>
2024-10-17 23:18:56 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String [] lessons={"Java","Js","English"};
+        MyClass person1=new MyClass("enes","marat uulu",20);
+        MyClass person2=new MyClass(lessons,"Fried chicken");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Person 1 name: "+person1.name+"\nPerson 1 surname: "+person1.surname+"\nPerson 1 age: "+person1.age);
+        System.out.println("Person 2 lessons: "+ Arrays.toString(person2.lessons)+"\nPerson 2 favourite food: "+person2.favFood);
     }
 }
